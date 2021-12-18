f3a4fb215074d7965482ce065fe61a2703a7f2c9
jgarciad3v <julio.garcia@bmvglobal.net>
2021-12-17 23:15:06 -0600

    add edit action

--- src/actions/todoAction.js
+++ src/actions/todoAction.js
@@ -1,5 +1,6 @@
 const ADDTASK = 'ADDTASK';
 const DELETETASK = 'DELETETASK'
+const EDIT = 'EDIT'
 
 export const addTask = value => ({
   type: ADDTASK,
@@ -12,5 +13,13 @@ export const addTask = value => ({
 export const deleteTask = id => ({
   type: DELETETASK,
   id
+})
+
+export const editTask = value => ({
+  type: EDIT,
+  data: {
+    id: value.id,
+    task: value.task
+  }
 })
 
